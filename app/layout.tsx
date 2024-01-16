2f91c26987336e41a0ced08d5f6d1c78104c0b3e
rael395 <118163137+rael395@users.noreply.github.com>
2024-01-16 14:12:05 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import Head from 'next/head';
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navigation from "../components/ui/Navigation";
 import Footer from "../components/ui/Footer";
-
+import Chatbot from "../components/ui/Chatbot";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,17 +12,22 @@ export const metadata = {
   description: "Insurance Agent for Hollard ",
 };
 
-export default function RootLayout({children }: {children: any}) {
+export default function RootLayout({ children }: {children: any}) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        {/* Add the external script here */}
+        <script defer src="https://openai-widget.web.app/ChatComponent.bundle.js"></script>
+      </Head>
       <body className={`${inter.className} text-body leading-relaxed`}>
         <Navigation />
         {children}
         <Footer />
-        
-       
-            
+        <Chatbot />
       </body>
-    </html>
+    </>
   );
 }
