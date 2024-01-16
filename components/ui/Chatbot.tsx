27aaa3166783d920901ab1ab646777be34b17d2d
rael395 <118163137+rael395@users.noreply.github.com>
2024-01-16 14:19:39 +0200

    Update Chatbot.tsx

--- components/ui/Chatbot.tsx
+++ components/ui/Chatbot.tsx
@@ -1,11 +1,23 @@
 'use client';
+// components/ui/Chatbot.js
+
 import React, { useEffect } from 'react';
+
+// Declare ChatComponent on the window object
+declare global {
+  interface Window {
+    ChatComponent: {
+      init: (param1: string, param2: string, options?: any) => void; // Adjust the types as per documentation
+      // Add any other methods or properties if needed
+    };
+  }
+}
 
 const Chatbot = () => {
   useEffect(() => {
     const initializeChatComponent = () => {
       // Check if the chat container exists
-      var chatContainer = document.getElementById('chat-container');
+      let chatContainer = document.getElementById('chat-container');
       
       // If the chat container doesn't exist, create it
       if (!chatContainer) {
