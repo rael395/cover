dbabf9cd63017cd8a3151d57588811868f84ab24
rael395 <118163137+rael395@users.noreply.github.com>
2024-01-20 12:02:52 +0000

    updated travel photo

--- app/services/[slug]/ServiceContent.tsx
+++ app/services/[slug]/ServiceContent.tsx
@@ -48,7 +48,7 @@ const ServiceContent: React.FC<{ service: any }> = ({ service }) => {
             </div>
 
             <div className='mb-16' >
-                <img src={service.image} width={1065} height={644}  alt="image"
+                <Image src={service.image} width={1065} height={644}  alt="image"
                 className='object-cover object-top'
                 />
             </div>
